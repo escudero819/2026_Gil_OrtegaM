495bf6042103214577ac45b1b2f786304deb6c3a
Mario Ortega_117 <mbo.bautista.mario@gmail.com>
2026-03-12 21:49:05 -0300

    heladeria a pantalla completa y 1 boton mal echo =)

--- heladeria.py
+++ heladeria.py
@@ -12,15 +12,16 @@ ancho = root.winfo_screenwidth()
 alto  = root.winfo_screenheight()
 
 #tama;o de los botones 
-ANCHO_BOTONES = 20
-ALTO_BOTONES = 5
+ANCHO_BOTONES = ancho//70
+ALTO_BOTONES = alto//60
+
 
 ventana = tk.Tk()
 ventana.geometry(f"{ancho}x{alto}")
 
 #botones
-
-
+boton_modo_elementos = tk.Button(ventana, text="Modo Elementos", width=ANCHO_BOTONES, height=ALTO_BOTONES)
+boton_modo_elementos.pack()
 
 #loop
 ventana.mainloop()
