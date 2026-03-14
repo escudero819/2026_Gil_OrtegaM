97d82f9c4c5f3c44d0b6e99f28589f035f90a7e4
Matias <mngilbarbosa@gmail.com>
2026-03-14 20:31:06 -0300

    archivo de pruebas y objeto pedido

--- pedido.py
+++ pedido.py
@@ -1,4 +1,11 @@
+"""
+ objeto pedido encargado de gestionar los helados de un recibo y la compra actual,  tiene metodos para añadir, eliminar y ver el pedido
+ ademas de poder armar un txt con el ticket y precio total
+
+"""
 class Pedido():
+    
+    PATH = "ventas/"
     
     def __init__(self, nro_pedido):
         self.nro_pedido = nro_pedido
@@ -12,9 +19,7 @@ class Pedido():
         self.ID_actual += 1
     
     def Eliminar(self, ID_eliminar):
-        print(self.lista_productos)
         for ID_loop in range(len(self.lista_Ids)):
-            print(ID_loop)
             if self.lista_Ids[ID_loop] == ID_eliminar:
                 self.lista_productos.pop(ID_eliminar)
                 self.lista_Ids.pop(ID_loop)
@@ -27,4 +32,23 @@ class Pedido():
             print(linea)
             lista_mostrar.append(linea)
         return lista_mostrar
-            
+    
+    def _PrecioTotal(self):
+        precio_total = 0
+        for ID_loop in range(len(self.lista_Ids)):
+            precio_total += self.lista_productos[self.lista_Ids[ID_loop]]["precio"]
+        return precio_total
+    
+    def Ticket(self):
+        archivo = self.PATH + "ticket"+str(self.nro_pedido)+".txt"
+        with open(archivo, "w") as archivo:
+            # Cabecera
+            archivo.write("Ticket Nro: " + str(self.nro_pedido) + "\n")
+            # Lista de productos
+            for ID_loop in range(len(self.lista_Ids)):
+                producto = self.lista_productos[self.lista_Ids[ID_loop]]
+                linea = str(ID_loop) + "- " + str(producto["nombre"]) + ": $" + str(producto["precio"]) + "\n"
+                archivo.write(linea)
+            # Pie con el total
+            archivo.write("-"*20 + "\n")
+            archivo.write("Precio Total: " + str(self._PrecioTotal()) + "\n")
--- pruebas.py
+++ pruebas.py
@@ -1,5 +1,7 @@
+import modo_stock
 from pedido import Pedido
 import modo_stock
+"""
 pedido_actual = Pedido(1)
 orden1 = {
     "nombre": "1 bocha",
@@ -13,5 +15,6 @@ orden2 = {
 }
 pedido_actual.Añadir(orden1)
 pedido_actual.Añadir(orden2)
-pedido_actual.Eliminar(0)
-lista_resultado = pedido_actual.VerPedido()
+pedido_actual.Ticket()
+"""
+modo_stock.Modo_stock(tk.Tk(), "400x400")
