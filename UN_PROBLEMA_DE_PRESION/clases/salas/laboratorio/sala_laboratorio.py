91e2b2c524aedce8ad5152087d95ad695580116c
matiasGil <mngilbarbosa@gmail.com>
2026-08-18 18:12:03 -0300

    comienzo de la sala laboratorio, modificar el archivo manager para poder probar de primeras cada sala

---
+++ UN_PROBLEMA_DE_PRESION/clases/salas/laboratorio/sala_laboratorio.py
@@ -0,0 +1,10 @@
+"""
+CLASE SALA HIDROPONIA HIJA DE SALA_BASE E INICIALIZACION DE LOS INTERACTUABLES
+"""
+
+# --- IMPORTACIONES ---
+
+# dependencias
+import os
+import arcade
+from configuraciones import Constantes as const
